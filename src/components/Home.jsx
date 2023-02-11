5758bf95c12174b85a1a020d1e2a70c7dd56ddcf
Alfredo Cruz <arandevs22@gmail.com>
2023-02-11 17:15:34 -0600

    home commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -31,6 +31,39 @@ function Home() {
                 </div>
                 <ul className="uk-slider-nav uk-dotnav uk-flex-center uk-margin"></ul>
             </div>
+            <div className="uk-margin-left" data-uk-slider>
+                <div className="uk-clearfix uk-margin-small-bottom">
+                    <div className="uk-float-right">
+                        <span></span>
+                    </div>
+                    <div className="uk-float-left">
+                        <span>Game Boy Advance</span>
+                    </div>
+                </div>
+                <ul className="uk-slider-items uk-child-width-1-4 uk-grid slider">
+                    <li>
+                        <img className="cover" src="https://cdn2.steamgriddb.com/file/sgdb-cdn/thumb/a9fcbba7da26095ce5010a39b8a36509.jpg" alt="" />
+                    </li>
+                    <li>
+                        <img className="cover" src="https://cdn2.steamgriddb.com/file/sgdb-cdn/thumb/a7843ff15ebf9f9b7ceda39eb8b1f117.jpg" alt="" />
+                    </li>
+                    <li>
+                        <img className="cover" src="https://cdn2.steamgriddb.com/file/sgdb-cdn/thumb/f3fd691d421ff0115dfc5310d4d3e701.jpg" alt="" />
+                    </li>
+                    <li>
+                        <img className="cover" src="https://cdn2.steamgriddb.com/file/sgdb-cdn/thumb/4ae5a92484155bfad3c7629a192244fe.jpg" alt="" />
+                    </li>
+                    <li>
+                        <img className="cover" src="https://cdn2.steamgriddb.com/file/sgdb-cdn/thumb/a687f78b9842c87225e74921765feb1b.jpg" alt="" />
+                    </li>
+                    <li>
+                        <img className="cover" src="https://cdn2.steamgriddb.com/file/sgdb-cdn/thumb/83d788e65786046542c9354d17b92450.jpg" alt="" />
+                    </li>
+                    <li>
+                        <img className="cover" src="https://cdn2.steamgriddb.com/file/sgdb-cdn/thumb/29ab124d059e51600744303ec829069e.jpg" alt="" />
+                    </li>
+                </ul>
+            </div>
         </div>
     )
 }
