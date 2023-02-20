ed7c98fef6907affc3423b89f774f0a9b5eb47b7
Alfredo Cruz <arandevs22@gmail.com>
2023-02-19 20:16:12 -0600

    firebase commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routes'
+import './firebase/firebaseConfig.js'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
